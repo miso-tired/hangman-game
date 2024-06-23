5c846e88ce859d18a5e24d18beb61474d475f6a2
Anthony <anthonylsa12@gmail.com>
2024-06-23 13:45:07 -0700

    Beginning to add body parts that will generate on wrong guesses.

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,22 @@ import { GallowsAndFigure } from "./components/GallowsAndFigure";
 
 
 function App() {
+  // list words from list.json
   const words = wordList.words;
 
-
+  // make array
   const [guessWord, setGuessWord] = useState(() => {
+    // randomize word picked from word array
     return words[Math.floor(Math.random() * words.length)];
   });
 
-
+  // for letters that have been used 
   const [usedLetters, setUsedLetters] = useState([]);
 
-
+  // Check if random word generates
   console.log(guessWord);
+
+  
   return (
     <div className="gamebox">
       <div className="status">
--- src/components/GallowsAndFigure.tsx
+++ src/components/GallowsAndFigure.tsx
@@ -1,7 +1,14 @@
+// Make body parts variables. Should allow body parts to render, based on guesses.
+const head = (
+    <div className="da-head"></div>
+)
+
+// Make Gallows for the gameboard.
 export function GallowsAndFigure () {
     return (
         <div className="gallows-container">
-            <div className="the-gallows" style={{ position: 'relative' }}>
+            {head}
+            <div className="the-gallows">
                 <div className="hanging-bar"></div>
                 <div className="top-bar"></div>
                 <div className="vert-bar"></div>
