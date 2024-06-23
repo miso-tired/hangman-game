b6984f03f717488a7624cf338e6945f4263549c8
Anthony <anthonylsa12@gmail.com>
2024-06-22 23:57:28 -0700

    Made the gallows for the hangman. :P

--- src/components/GallowsAndFigure.tsx
+++ src/components/GallowsAndFigure.tsx
@@ -1,7 +1,10 @@
 export function GallowsAndFigure () {
     return (
         <div className="gallows-container">
-            <div className="the-gallows">
+            <div className="the-gallows" style={{ position: 'relative' }}>
+                <div className="hanging-bar"></div>
+                <div className="top-bar"></div>
+                <div className="vert-bar"></div>
                 <div className="gallows-base"></div>
             </div>
         </div>
