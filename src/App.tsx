c6e0439f2d6e736fead8d4f7fde1344f147433bf
Anthony <anthonylsa12@gmail.com>
2024-06-24 17:27:30 -0700

    '/game' route works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,15 @@
 // Imports
-import { useCallback, useEffect, useState } from "react";
-import wordList from './list.json';
-import '../src/styles/game.css'
-import { GallowsAndFigure } from "./components/GallowsAndFigure";
-import { Word } from "./components/Word";
-import { Keys } from "./components/Keys";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Game } from "./components/Game";
 
 
 function App() {
-  // list words from list.json
-  const words = wordList.words;
-
-  // make array
-  const [guessWord] = useState(() => {
-    // randomize word picked from word array
-    return words[Math.floor(Math.random() * words.length)];
-  });
-
-  // for letters that have been used 
-  const [usedLetters, setUsedLetters] = useState<string[]>([]);
-
-  const wrongLetters = usedLetters.filter(letter => !guessWord.includes(letter))
-
-  const intro = "What word is it?"
-  const loss = wrongLetters.length >= 6
-  const win = guessWord.split("").every(letter => usedLetters.includes(letter))
-
-  const addUsedLetter = useCallback((letter: string) => {
-    if (usedLetters.includes(letter) || win || loss) return
-
-    setUsedLetters(currentLetters => [...currentLetters, letter])
-  }, [usedLetters, win, loss])
-
-  useEffect(() => {
-    const handler = (e: KeyboardEvent) => {
-      const key = e.key
-
-      if (!key.match(/^[a-z]$/)) return
-
-      e.preventDefault()
-      addUsedLetter(key)
-    }
-    document.addEventListener("keypress", handler)
-
-    return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [usedLetters])
-  
-  console.log(guessWord)
-
   return (
-    <div className="gamebox">
-      <div className="status">
-        {!win && !loss && intro}
-        {win && "You Win! Refresh for a new game."}
-        {loss && "Do Better. Refresh for a new game"}
-      </div>
-      <GallowsAndFigure guessAmount={wrongLetters.length} />
-      <Word 
-      reveal={loss}
-      usedLetters={usedLetters} 
-      guessWord={guessWord} />
-      <Keys 
-      disabled={win || loss}
-      activeLetters={usedLetters.filter(letter => guessWord.includes(letter))} 
-      inactiveLetters={usedLetters} 
-      addUsedLetter={addUsedLetter} />
-    </div>
+      <Router>
+      <Routes>
+        <Route path='/game' element={<Game />} />
+      </Routes>
+      </Router>
   )
 }
 
