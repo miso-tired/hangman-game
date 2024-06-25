bac45bedc088ee834bab2ee29af0939d8b8913db
Anthony <anthonylsa12@gmail.com>
2024-06-24 17:46:53 -0700

    Making landing page for the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 // Imports
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Game } from "./pages/Game";
-
+import Home from "./Home";
 
 function App() {
   return (
       <Router>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/game' element={<Game />} />
       </Routes>
       </Router>
