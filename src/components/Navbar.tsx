3b5013c4165b1ad6b040f34270478bbcdbc9b0d6
Anthony <anthonylsa12@gmail.com>
2024-06-29 10:55:29 -0700

    More styling for the navbar and working on Login and Register pages.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from "react"
 import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
 
 const Navbar = () => {
     return (
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/game">Game</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
+        <ul className="list">
+          <li className='home-link'><Link to="/">Home</Link></li>
+          <li className='game-link'><Link to="/game">Game</Link></li>
+          <li className='login-link'><Link to='/login'>Login</Link></li>
+          <li className='register-link'><Link to='/register'>Register</Link></li>
         </ul>
       </nav>
     );
--- src/users/SignUpForm.tsx
+++ src/users/SignUpForm.tsx
@@ -1,57 +1,59 @@
-// Imports 
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'
+// Imports
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignUpForm() {
-    
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const [user, setUser] = useState({
-        name: '',
-        username: '',
-        password: ''
-    })
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    password: "",
+  });
 
-    // Form submission logic
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
+  // Form submission logic
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
 
-        // Redirect to home page after sign up
-        navigate('/')
-    }
+    // Redirect to home page after sign up
+    navigate("/");
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input 
-                    type="text" 
-                    id="name" 
-                    value={user.name} 
-                    onChange={(e) => setUser({ ...user, name: e.target.value })}  
-                />
-            </div>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input 
-                    type="text" 
-                    id="lastName" 
-                    value={user.username} 
-                    onChange={(e) => setUser({ ...user, username: e.target.value })}  
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input 
-                    type="password" 
-                    id="password" 
-                    value={user.password} 
-                    onChange={(e) => setUser({ ...user, password: e.target.value })}
-                />
-            </div>
-            <button type="submit">Sign Up</button>
-        </form>
-    );
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={user.name}
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="lastName"
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
 }
 
-export default SignUpForm
+export default SignUpForm;
