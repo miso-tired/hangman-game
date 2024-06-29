035976d4e0a930326f78ebc889f7587ca09f776e
Anthony <anthonylsa12@gmail.com>
2024-06-29 09:38:11 -0700

    Working on navbar. Added Navbar component above the routes so the navbar displays on all pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 // Imports
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Game } from "./pages/Game";
-import SignUpForm  from './users/SignUpForm'
+import Navbar from "./components/Navbar";
 import Home from "./Home";
+import { Game } from "./pages/Game";
+import SignUpForm from "./users/SignUpForm";
 import LoginForm from "./users/LoginForm";
 
 function App() {
   return (
       <Router>
+        <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/game' element={<Game />} />
-        <Route path='/login' element={<LoginForm />} />
         <Route path='/register' element={<SignUpForm />} />
+        <Route path='/login' element={<LoginForm />} />
       </Routes>
       </Router>
   )
--- src/Default.tsx
+++ src/Default.tsx
@@ -8,20 +8,7 @@ interface DefProps {
 const Def: React.FC<DefProps> = ({ children }) => {
     return (
         <>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href='/game'>Play Now</a>
-                        </li>
-                        <li>
-                            <a href='/login'>Login</a>
-                        </li>
-                        <li>
-                            <a href='/register'>Register</a>
-                        </li>
-                    </ul>
-                </nav>
-                {children}
+       {children}
         </>
     );
 };
